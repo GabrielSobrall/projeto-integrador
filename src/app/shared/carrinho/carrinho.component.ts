fcc135cc68d7a48c5904e79b257bb38089bcd71b
gabrielsobrall <gabrielsobral2018@outlook.com>
2025-05-23 18:27:19 -0300

    teste

--- src/app/shared/carrinho/carrinho.component.ts
+++ src/app/shared/carrinho/carrinho.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.css'
 })
