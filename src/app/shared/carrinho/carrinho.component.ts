7d850c0e80ab14689b699d4ae6f748c3d3a97b1d
gabrielsobrall <gabrielsobral2018@outlook.com>
2025-05-25 08:31:51 -0300

    TESTEX

--- src/app/shared/carrinho/carrinho.component.ts
+++ src/app/shared/carrinho/carrinho.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService, Produto } from '../../services/carrinho.service';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
   templateUrl: './carrinho.component.html',
-  styleUrl: './carrinho.component.css'
+  styleUrls: ['./carrinho.component.scss']
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+  carrinho: Produto[] = [];
+  total: number = 0;
 
+  constructor(private carrinhoService: CarrinhoService) {}
+
+  ngOnInit(): void {
+    this.carregarCarrinho();
+  }
+
+  carregarCarrinho(): void {
+    this.carrinhoService.getItens().subscribe(itens => this.carrinho = itens);
+    this.carrinhoService.getTotal().subscribe(valor => this.total = valor);
+  }
+
+  alterarQuantidade(id: number, delta: number): void {
+    this.carrinhoService.alterarQuantidade(id, delta).subscribe(() => {
+      this.carregarCarrinho();
+    });
+  }
+
+  removerItem(id: number): void {
+    this.carrinhoService.remover(id).subscribe(() => {
+      this.carregarCarrinho();
+    });
+  }
+
+  finalizarCompra(): void {
+    alert('Compra finalizada!');
+    this.carrinhoService.limparCarrinho().subscribe(() => {
+      this.carregarCarrinho();
+    });
+  }
 }
